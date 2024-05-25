28e6648261001a5cbd8112ca650d288ba0adddbe
MaximStanko <f.maxim.stanko@gmail.com>
2024-05-25 12:40:30 +0200

    enemy pushback

--- scripts/enemy_prototype.gd
+++ scripts/enemy_prototype.gd
@@ -2,9 +2,12 @@ extends CharacterBody2D
 
 signal hit(body)
 
-const SPEED = 4000
-var damage_slow = 1
-var hp = 100
+const group_name = "enemy"
+
+@export var SPEED = 4000
+@export var enemy_pushback = 300
+@export var damage_slow = 1
+@export var hp = 100
 
 # Reference muss geändert werden
 @onready var hilation = %hilation
@@ -13,11 +16,26 @@ var hp = 100
 @onready var timer_player_hit = $TimerPlayerHit
 
 var player
+var nearest_enemy
+var lowest_distance
+var distance
+var pushback_vector
 var lastVel
 var step_back = false
 
+func _ready():
+	add_to_group(group_name)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	lowest_distance = INF
+	for member in get_tree().get_nodes_in_group(group_name):
+		if member != self:
+			distance = (position - member.position).length()
+			if distance < lowest_distance:
+				lowest_distance = distance
+				nearest_enemy = member
+	
 	if (hilation.position - position).length() < (anni.position - position).length():
 		player = hilation
 	else:
@@ -28,6 +46,10 @@ func _physics_process(delta):
 		velocity = -dir.normalized() * SPEED * delta * 2
 	else:
 		velocity = dir.normalized() * SPEED * delta * damage_slow
+	
+	pushback_vector = (enemy_pushback * Vector2(1,1)) / (position - nearest_enemy.position)
+	velocity += pushback_vector
+	
 	move_and_slide()
 
 func _on_player_detector_body_entered(body):
